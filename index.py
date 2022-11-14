92b6f41c5183972520fa42d3ecb48d97ad437302
jeff103029 <jeff103029@gmail.com>
2022-11-14 16:57:22 +0800

    make it better

--- index.py
+++ index.py
@@ -29,9 +29,9 @@ def account():
         for doc in docs:
             dict = doc.to_dict()
             if cond in dict["Course"] and cond1 in dict["Leacture"]:
-                #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
-            result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
-            result += dict["Time"] + "於" + dict["Room"] + "上課<br>"
+            #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
+                result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
+                result += dict["Time"] + "於" + dict["Room"] + "上課<br>"
 
         if result =="":
             result="sorry...."
