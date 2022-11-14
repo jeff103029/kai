cea85481890720ebc1ddf4ee7bb5553a340bd3e8
jeff103029 <jeff103029@gmail.com>
2022-11-14 16:32:33 +0800

    make it better

--- index.py
+++ index.py
@@ -19,19 +19,19 @@ def account():
         cond = request.form["leac"]
         cond1= request.form["teac"]
         result = "你的關鍵字的課程為:：" +cond
-        result = "你的關鍵字的課程為:：" +cond1
+        result += "你的老師關鍵字為:：" +cond1
 
 
-        db = firestore.client()
-        collection_ref = db.collection("111")
-        docs = collection_ref.get()
-        result = ""
-        for doc in docs:
-            dict = doc.to_dict()
-            if cond in dict["Course"] and cond1 in dict["Leacture"]:
+        #db = firestore.client()
+        #collection_ref = db.collection("111")
+        #docs = collection_ref.get()
+        #result = ""
+        #for doc in docs:
+            #dict = doc.to_dict()
+           # if cond in dict["Course"] and cond1 in dict["Leacture"]:
                 #print("{}老師開的{}課程,每週{}於{}上課".format(dict["Leacture"], dict["Course"],  dict["Time"],dict["Room"]))
-                result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
-                result += dict["Time"] + "於" + dict["Room"] + "上課<br>"
+                #result += dict["Leacture"] + "老師開的" + dict["Course"] + "課程,每週"
+                #result += dict["Time"] + "於" + dict["Room"] + "上課<br>"
 
         if result =="":
             result="sorry...."
